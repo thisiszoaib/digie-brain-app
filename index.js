54f3476de71e6c85f2c95ef172182f62a7f843cc
Zoaib Ahmed Khan <zoaib.ahmed@techlogix.com>
2017-05-29 11:13:41 +0500

    Added get function

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+restService.get('/',function(req,res)
+{
+	res.send('Hello World')
+})
+
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
