0c4ff4af2a569dd38fa227e52189bec39d0435c3
Zoaib Ahmed Khan <zoaib.ahmed@techlogix.com>
2017-06-05 11:32:38 +0500

    Added simple cache to store authentication info for same API.AI session

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const restService = express();
 const bodyParser = require('body-parser');
 const request = require('request');
+const nodeCache = require('node-cache');
+const myCache = new nodeCache();
 
 var optionsAuthenticate = {
   url: 'https://mydigie.com/php/authenticateUser.php',
@@ -25,30 +27,61 @@ restService.get('/', function (req, res) {
   res.send('Hello World.')
 })
 
-restService.post('/echo', function (req, res) {
-  var isStatementThere = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText;
+restService.post('/processStatement', function (req, res) {
+  var isStatementThere = req.body.result && req.body.result.parameters && req.body.result.parameters.statement;
 
   if (isStatementThere) {
 
-    var statement = req.body.result.parameters.echoText;
+    var statement = req.body.result.parameters.statement;
 
-    request(optionsAuthenticate, function (error, response, body) {
-      var authenticationResponse = body;
-      authenticationResponse = authenticationResponse.replace(/[\n\r\t]/g, ' ');
-      authenticationResponse = authenticationResponse.trim();
+    var apiAiSessionId = req.body.sessionId;
 
-      var separatedResponse = authenticationResponse.split("-");
-      var sessionId = separatedResponse[0].substring(13).trim();
+    var digieSessionId = myCache.get(apiAiSessionId);
 
+    if (digieSessionId == undefined) {
+        request(optionsAuthenticate, function (error, response, body) {
+        var authenticationResponse = body;
+        authenticationResponse = authenticationResponse.replace(/[\n\r\t]/g, ' ');
+        authenticationResponse = authenticationResponse.trim();
+
+        var separatedResponse = authenticationResponse.split("-");
+        var sessionId = separatedResponse[0].substring(13).trim();
+
+        myCache.set(apiAiSessionId,sessionId);
+
+        var processStatementOptions = {
+          url: 'https://mydigie.com/php/processStatement.php',
+          headers: {
+            'Cookie': 'PHPSESSID=' + sessionId,
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          method: 'POST',
+          body: 'SessionID=' + sessionId + '&Language=English&DBName=myDigie&Statement=' + statement,
+          rejectUnauthorized: false
+        };
+
+        request(processStatementOptions, function (error, response, body) {
+
+          var speech = body.replace(/[\n\r\t]/g, ' ').trim().substring(15);
+
+          return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'digie-brain-app'
+          });
+        });
+      });
+    } else {
       var processStatementOptions = {
         url: 'https://mydigie.com/php/processStatement.php',
         headers: {
-          'Cookie': 'PHPSESSID=' + sessionId,
+          'Cookie': 'PHPSESSID=' + digieSessionId,
           'Connection': 'keep-alive',
           'Content-Type': 'application/x-www-form-urlencoded'
         },
         method: 'POST',
-        body: 'SessionID=' + sessionId + '&Language=English&DBName=myDigie&Statement=' + statement,
+        body: 'SessionID=' + digieSessionId + '&Language=English&DBName=myDigie&Statement=' + statement,
         rejectUnauthorized: false
       };
 
@@ -59,18 +92,17 @@ restService.post('/echo', function (req, res) {
         return res.json({
           speech: speech,
           displayText: speech,
-          source: 'webhook-echo-sample'
+          source: 'digie-brain-app'
         });
       });
+    }
 
 
-
-    });
   } else {
     return res.json({
-      speech: 'No input was sent.',
-      displayText: 'No input was sent.',
-      source: 'webhook-echo-sample'
+      speech: 'Please say something. Couldnt get that',
+      displayText: 'Please say something. Couldnt get that',
+      source: 'digie-brain-app'
     });
   }
 
